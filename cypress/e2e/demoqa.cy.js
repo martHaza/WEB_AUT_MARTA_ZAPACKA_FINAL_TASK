830785add114c9e45dc058153da64dfb8f73654f
martHaza <martazapacka17@gmail.com>
2025-05-26 13:13:17 +0300

    iesākts demoqa

--- cypress/e2e/demoqa.cy.js
+++ cypress/e2e/demoqa.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import { HomePage } from "../pageObjects/homePage";
+
+describe('Demoqa scenario', () => {
+  it('Demoqa scenario ', () => {
+    HomePage.visit();
+    const name = "Marta";
+    const lastName = "Zapacka";
+    const email = "s24zapamart@venta.lv";
+    const mobileNumber = "2134567891";
+    const currentAddress = "zupa 13";
+
+    HomePage.firstName.type(name);
+    HomePage.lastName.type(lastName);
+    HomePage.email.type(email);
+    HomePage.femaleRadio.type();
+    HomePage.mobileNumber.type(mobileNumber);
+    HomePage.currentAddress.type(currentAddress);
+    
   })
 })
